8dd346022e55f6094dd6a50c7ab073303ed7ed85
leydera10 <leiderasis30@gmail.com>
2023-09-12 21:10:04 -0500

    Primera Entrega

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const productsRouter = require('./src/routes/products.router');
+const cartsRouter = require('./src/routes/carts.router');
+const app = express();
+const PORT = 8080;
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.use('/', productsRouter);
+app.use('/', cartsRouter);
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
